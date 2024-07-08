f9c262a4cad0ca896fb4872fb84cb921d1072be1
徐亚军 <850674935@qq.com>
2024-07-08 10:43:15 +0800

    feat(init): init

---
+++ src/pages/mp/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <view>mp home</view>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/pc/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>My Vue Component <text>!!!</text>111 </h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+h1 {
+  color: blue;
+  text{
+    color: red;
+  }
+}
+</style>

3184c9c6fc14dcb8a54bcc7827a7880e925d3045
付宇威 <f6512098@163.com>
2024-07-15 10:34:18 +0800

    update

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,13 +5,10 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 import '@vue/runtime-core'
 
-export {}
+export {};
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    AppFooter: typeof import('./components/AppFooter.vue')['default']
-    AppLogos: typeof import('./components/AppLogos.vue')['default']
-    HiCounter: typeof import('./components/HiCounter.vue')['default']
-    InputEntry: typeof import('./components/InputEntry.vue')['default']
+    PageNav: (typeof import('./components/page-nav/index.vue'))['default'];
   }
 }

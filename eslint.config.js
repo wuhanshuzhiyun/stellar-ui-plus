1039286d5b32deb69872c15c85ec5b622c14f142
付宇威 <f6512098@163.com>
2024-07-23 16:52:54 +0800

    utils添加sleep方法

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,6 @@ const uni = require('@uni-helper/eslint-config')
 module.exports = uni({
   rules: {
     'no-console': 'off',
+    'no-unexpected-multiline': 'off', // 或 'warn'，如果你不希望此规则严格生效
   },
 })

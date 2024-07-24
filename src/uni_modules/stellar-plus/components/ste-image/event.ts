e13b50121ce77d548cad8d0f7c1378fa448facc0
徐亚军 <850674935@qq.com>
2024-07-24 16:17:12 +0800

    feat(save): 添加hbuild支持模板

--- src/uni_modules/stellar-plus/components/ste-image/event.ts
+++
@@ -1,7 +0,0 @@
-import { defineEmits, withDefaults } from 'vue'
-
-export default withDefaults(defineEmits<{
-  click?: (e?: Event) => void
-  load?: (e?: Event) => void
-  error?: (e?: Event) => void
-}>(), {})

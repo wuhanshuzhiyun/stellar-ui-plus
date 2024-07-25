564871fb4d1e5774487d5cc9ab26a3576804674b
付宇威 <f6512098@163.com>
2024-07-25 15:31:29 +0800

    HBuilder提示配置文件添加

--- src/uni_modules/stellar-plus/components/ste-text/props.ts
+++ src/uni_modules/stellar-plus/components/ste-text/props.ts
@@ -1,6 +1,10 @@
+import type { PropType } from 'vue'
+
+type TextSpace = 'ensp' | 'emsp' | 'nbsp'
+
 export default {
   selectable: { type: Boolean, default: false },
-  space: { type: String, default: '' },
+  space: { type: String as PropType<TextSpace>, default: undefined },
   decode: { type: Boolean, default: false },
   lines: { type: Number, default: undefined },
 }

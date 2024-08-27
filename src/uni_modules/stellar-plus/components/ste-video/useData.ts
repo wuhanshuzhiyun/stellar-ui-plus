cee1f3eab54ea0a2c48d7e253661b8c60ea4effa
付宇威 <f6512098@163.com>
2024-08-27 17:34:20 +0800

    修复视频组件在小程序全屏异常

--- src/uni_modules/stellar-plus/components/ste-video/useData.ts
+++ src/uni_modules/stellar-plus/components/ste-video/useData.ts
@@ -4,7 +4,6 @@ import type { BaseEvent } from '../../types/event'
 import type { VideoEmits, VideoProps } from './props'
 
 export default function useData(_props: VideoProps, emits: SetupContext<VideoEmits>['emit']) {
-  const reRenderFlag = ref(true)
   const videoSrc = ref('')
   const playState = ref(false)
   const isFull = ref(false)
@@ -68,6 +67,7 @@ export default function useData(_props: VideoProps, emits: SetupContext<VideoEmi
 
   // 格式化视频时间为时分秒
   function formatTime(seconds: number) {
+    // console.log('formatTime is ', seconds);
     const hours = Math.floor(seconds / 3600)
     const minutes = Math.floor((seconds % 3600) / 60)
     const remainingSeconds = Math.floor(seconds % 60)
@@ -78,12 +78,10 @@ export default function useData(_props: VideoProps, emits: SetupContext<VideoEmi
 
     if (formattedHours !== '')
       return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`
-    else
-      return `${formattedMinutes}:${formattedSeconds}`
+    else return `${formattedMinutes}:${formattedSeconds}`
   }
 
   return {
-    reRenderFlag,
     videoSrc,
     playState,
     isFull,

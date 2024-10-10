403d1439c88eb7dc5dd16b07d5c84961713feea1
徐亚军 <850674935@qq.com>
2024-10-10 10:27:33 +0800

    feat(save): 网站名称修复

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import App from './App.vue'
 
 export function createApp() {
   const app = createSSRApp(App)
+  // #ifdef H5
+  setTimeout(() => {
+    const title = document.head.querySelector('title')
+    if (!title)
+      return
+    title.textContent = 'Stellar Plus'
+  })
+  // #endif
   return {
     app,
   }

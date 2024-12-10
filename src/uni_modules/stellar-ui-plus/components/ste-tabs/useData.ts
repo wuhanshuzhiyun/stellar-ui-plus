0e87650c8dc556b703b6fe9820be1298fec5add7
徐亚军 <850674935@qq.com>
2024-12-10 11:53:52 +0800

    feat(ste-tabs): 修复标签页均分标签为0时的BUG

--- src/uni_modules/stellar-ui-plus/components/ste-tabs/useData.ts
+++ src/uni_modules/stellar-ui-plus/components/ste-tabs/useData.ts
@@ -182,9 +182,9 @@ export default function useData({
     return { marginLeft, display }
   })
 
-  const cmpScrollX = computed(() => props.divideNum > 0 && internalChildren.length > props.divideNum)
+  const cmpScrollX = computed(() => internalChildren.length > props.divideNum)
 
-  const cmpPullDown = computed(() => cmpScrollX.value && props.pullDown)
+  const cmpPullDown = computed(() => props.divideNum > 0 && cmpScrollX.value && props.pullDown)
 
   const cmpPullStyle = computed(() => {
     if (!listBoxEl.value || !openPullDown.value)

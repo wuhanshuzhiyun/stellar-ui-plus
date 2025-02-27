4803d9c803aa6c549cff1dda6b9a6ceca6e70631
HEXIAYUE <q1506589520q@gmail.com>
2025-02-28 01:56:53 +0800

    去掉pinia

--- src/uni_modules/stellar-ui-plus/store/color.ts
+++ src/uni_modules/stellar-ui-plus/store/color.ts
@@ -1,24 +1,36 @@
-import { defineStore } from 'pinia'
 import { ref } from 'vue'
-
 // 主题色内容
-export const useColorStore = defineStore('color', () => {
+export function useColorStore() {
   const color = ref({
     steThemeColor: '#0090FF',
     defaultColor: '#0090FF',
   })
-  const setColor = (val: any) => {
-    console.log('val', val)
-    color.value = Object.assign(color.value, val)
-    console.log('color', color.value)
-  }
   const getColor = () => {
     return color.value
   }
-
   return {
     color,
-    setColor,
     getColor,
   }
-})
+}
+
+// defineStore('color', () => {
+//   const color = ref({
+//     steThemeColor: '#0090FF',
+//     defaultColor: '#0090FF',
+//   })
+//   const setColor = (val: any) => {
+//     console.log('val', val)
+//     color.value = Object.assign(color.value, val)
+//     console.log('color', color.value)
+//   }
+//   const getColor = () => {
+//     return color.value
+//   }
+
+//   return {
+//     color,
+//     setColor,
+//     getColor,
+//   }
+// })

ff582059f16d445f29bd655ee9753d154def9f7c
付宇威 <f6512098@163.com>
2025-03-07 11:26:15 +0800

    引入主题色

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createSSRApp } from 'vue';
 import App from './App.vue';
 
+// main.js
+import useColor from '@/uni_modules/stellar-ui-plus/config/color';
+
+const color = useColor();
+color.setColor({ steThemeColor: '#0090FF' });
+
 export function createApp() {
     const app = createSSRApp(App);
     // #ifdef H5

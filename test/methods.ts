8c5a0fbb12b8902ced4260c7f6eacfa1509bbfbb
ÇØÅô·É <q1506589520q@gmail.com>
2025-04-10 10:08:50 +0800

    添加插件补丁

--- test/methods.ts
+++ test/methods.ts
@@ -1,7 +1,7 @@
 /***
  * 解析自定义字体
- * @param  {string} fontSize 自定义字体
- * @return {string} 解析后的字体
+ * @param  {string} fontSize 自定义字体大小
+ * @return {string} 解析后的字体大小
  */
 export function fontSize(fontSize: string) {
     return Number(fontSize.split(',')[0].replace('var(--font-size-', '')) / 2 + 'px';

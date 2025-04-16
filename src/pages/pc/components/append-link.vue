fb9cc91543643edff6c61b523a52c5e019253cb3
付宇威 <f6512098@163.com>
2025-04-16 16:32:27 +0800

    网站添加github、gitee跳转

---
+++ src/pages/pc/components/append-link.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="append-link">
+        <a class="link-item" :style="itemStyle(item)" target="_blank" :href="item.url" :title="item.title" v-for="(item, k) in linkData" :key="k"></a>
+    </div>
+</template>
+
+<script lang="ts" setup>
+interface LinkData {
+    title: string;
+    src: string;
+    url: string;
+}
+const linkData: LinkData[] = [
+    {
+        title: 'github',
+        src: 'https://image.whzb.com/chain/StellarUI/image/github.png',
+        url: 'https://github.com/wuhanshuzhiyun/stellar-ui-plus',
+    },
+    {
+        title: 'gitee',
+        src: 'https://image.whzb.com/chain/StellarUI/image/gitee.png',
+        url: 'https://gitee.com/wuhanshuzhiyun/stellar-ui-plus',
+    },
+];
+
+const itemStyle = (item: LinkData) => {
+    return {
+        backgroundImage: `url(${item.src})`,
+    };
+};
+</script>
+<style lang="scss" scoped>
+.append-link {
+    .link-item {
+        width: 26px;
+        height: 26px;
+        vertical-align: middle;
+        background-repeat: no-repeat;
+        background-size: 26px;
+        display: inline-block;
+        line-height: 64px;
+        background-position: 0 -51px;
+        color: #ccc;
+    }
+
+    .link-item:not(:first-child) {
+        margin-left: 16px;
+    }
+}
+</style>

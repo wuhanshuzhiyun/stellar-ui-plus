322b84133086694d9eecfecf5521108d198c9427
付宇威 <f6512098@163.com>
2025-04-30 11:25:52 +0800

    修改使用示例

--- src/pages/mp/demo-views/line-chart/line-chart.vue
+++ src/pages/mp/demo-views/line-chart/line-chart.vue
@@ -5,33 +5,24 @@ let categories: any = ref([]);
 let series2: any = ref([]);
 let categories2: any = ref([]);
 
-onMounted(() => {
-    getServerData();
-});
-function getServerData() {
-    //模拟从服务器获取数据时的延时
-    setTimeout(() => {
-        // 模拟服务器返回数据，如果数据格式和标准格式不同，需自行按下面的格式拼接
-        series1.value = [
-            {
-                name: '目标值',
-                data: [35, 36, 31, 33, 13, 34],
-            },
-        ];
-        categories.value = ['2018', '2019', '2020', '2021', '2022', '2023'];
+series1.value = [
+    {
+        name: '目标值',
+        data: [35, 36, 31, 33, 13, 34],
+    },
+];
+categories.value = ['2018', '2019', '2020', '2021', '2022', '2023'];
 
-        series2.value = [
-            {
-                name: '目标值',
-                data: [35, 36, 31, 33, 13, 34],
-            },
-            {
-                name: '完成量',
-                data: [18, 27, 21, 24, 6, 28],
-            },
-        ];
-    }, 100);
-}
+series2.value = [
+    {
+        name: '目标值',
+        data: [35, 36, 31, 33, 13, 34],
+    },
+    {
+        name: '完成量',
+        data: [18, 27, 21, 24, 6, 28],
+    },
+];
 </script>
 
 <template>

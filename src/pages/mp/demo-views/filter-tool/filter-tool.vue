5cd6a95a03a4fc5e8224c6f4638beaf58bcfa5f5
付宇威 <f6512098@163.com>
2025-05-20 14:34:42 +0800

    初始化筛选选项组件和搜索组件

---
+++ src/pages/mp/demo-views/filter-tool/filter-tool.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+</script>
+
+<template>
+    <page-layout title="商品列表">
+        <view class="description">
+            <view class="cmp-name">FilterTool 筛选选项</view>
+            <view class="cmp-desc">可配置多选选项组件</view>
+        </view>
+        <view class="demo-item">
+            <view class="title">基础用法</view>
+        </view>
+    </page-layout>
+</template>
---
+++ src/pages/mp/demo-views/search-box/search-box.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+</script>
+
+<template>
+    <page-layout title="商品列表">
+        <view class="description">
+            <view class="cmp-name">SearchBox 搜索组件</view>
+            <view class="cmp-desc">集成多个功能的搜索组件</view>
+        </view>
+        <view class="demo-item">
+            <view class="title">基础用法</view>
+        </view>
+    </page-layout>
+</template>

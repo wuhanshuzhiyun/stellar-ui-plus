8faf516bf07c0ff30fe0fa449722d7652f49f5fb
徐亚军 <850674935@qq.com>
2025-06-04 11:26:38 +0800

    feat(save): save

--- src/uni_modules/stellar-ui-plus/components/ste-function-list/props.ts
+++ src/uni_modules/stellar-ui-plus/components/ste-function-list/props.ts
@@ -1,29 +1,41 @@
-import type { PropType } from 'vue'
-import type { DateType, SignType } from './date'
+import type { PropType } from 'vue';
+
+export type FunctionListItem = {
+    id: string | number;
+    title: string;
+    subhead?: string;
+    statusText?: string;
+    image?: string;
+    buttonText: string;
+    buttonIcon: string;
+};
+
+export type Props = {
+    background: string;
+    contentBg: string;
+    title: string;
+    subhead: string;
+    data: (FunctionListItem & { [key: string]: any })[];
+    emptyImage: string;
+    emptyText: string;
+    emptyButtonText: string;
+};
 
 export default {
-  title: { type: String, default: () => '日期选择' },
-  showTitle: { type: Boolean, default: () => true },
-  list: { type: Array as PropType<DateType[]>, default: () => [] },
-  mode: { type: String, default: () => 'single' },
-  startText: { type: String, default: () => '开始' },
-  endText: { type: String, default: () => '结束' },
-  color: { type: String, default: () => '' },
-  minDate: { type: [String, Number, Date], default: () => 0 },
-  maxDate: { type: [String, Number, Date], default: () => 0 },
-  maxCount: { type: [Number, String], default: () => 0 },
-  formatter: { type: String, default: () => 'YYYY-MM-DD' },
-  showMark: { type: Boolean, default: () => true },
-  readonly: { type: Boolean, default: () => false },
-  maxRange: { type: Number, default: () => null },
-  rangePrompt: { type: String, default: () => null },
-  showRangePrompt: { type: Boolean, default: () => true },
-  allowSameDay: { type: Boolean, default: () => false },
-  showConfirm: { type: Boolean, default: () => true },
-  width: { type: [Number, String], default: () => '100%' },
-  height: { type: [Number, String], default: () => '100%' },
-  signs: { type: Object as PropType<{ [key: string]: SignType }>, default: () => ({}) },
-  defaultDate: { type: [String, Number, Date], default: () => 0 },
-  monthCount: { type: Number, default: () => 12 },
-  weekendColor: { type: String, default: () => '' },
-}
+    /** 背景色 */
+    background: { type: String, default: () => '#fff' },
+    /** 内容区域背景 */
+    contentBg: { type: String, default: () => '#f9f9f9' },
+    /** 标题 */
+    title: { type: String, default: () => '' },
+    /** 副标题 */
+    subhead: { type: String, default: () => '' },
+    /** 列表数据 */
+    data: { type: Array as PropType<(FunctionListItem & { [key: string]: any })[]>, default: () => [] },
+    /** 列表为空时显示的图片 */
+    emptyImage: { type: String, default: () => '' },
+    /** 列表为空时显示的文字 */
+    emptyText: { type: String, default: () => '' },
+    /** 列表为空时显示的按钮文字 */
+    emptyButtonText: { type: String, default: () => '' },
+};
--- src/uni_modules/stellar-ui-plus/components/ste-function-list/ste-function-list.vue
+++ src/uni_modules/stellar-ui-plus/components/ste-function-list/ste-function-list.vue
@@ -1,8 +1,87 @@
 <script setup lang="ts">
 import { watch, onMounted, computed, nextTick } from 'vue';
+import propData from './props';
 import { useColorStore } from '../../store/color';
-let { getColor } = useColorStore();
-</script>
-<template></template>
 
-<style lang="scss" scoped></style>
+let { getColor } = useColorStore();
+
+const props = defineProps(propData);
+
+const rootStyle = computed(() => {
+    return {
+        background: props.background,
+        '--ste-function-list-content-bg': props.contentBg,
+    };
+});
+</script>
+<template>
+    <view class="ste-function-list-root" :style="[rootStyle]">
+        <view class="ste-function-list-header">
+            <view class="ste-function-list-header-l">
+                <view class="ste-function-list-title">{{ title }}</view>
+                <view class="ste-function-list-subhead">
+                    <slot name="subhead">
+                        {{ subhead }}
+                    </slot>
+                </view>
+            </view>
+            <view class="ste-function-list-header-r">
+                <slot name="header-right">
+                    <view class="ste-function-list-more">
+                        更多
+                        <ste-icon code="&#xe674;" color="#353535"></ste-icon>
+                    </view>
+                </slot>
+            </view>
+        </view>
+        <view class="ste-function-list-content">
+            <view class="content-list">
+                <view class="content-list-item" v-for="(item, index) in data" :key="index">
+                    <view class="content-list-item-image"></view>
+                    <view class="content-list-item-info">{{ item.title }}</view>
+                </view>
+            </view>
+        </view>
+    </view>
+</template>
+
+<style lang="scss" scoped>
+.ste-function-list-root {
+    padding: 24rpx 24rpx 20rpx 24rpx;
+    border-radius: 12rpx;
+    .ste-function-list-header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        .ste-function-list-header-l {
+            display: flex;
+            align-items: center;
+            .ste-function-list-title {
+                font-weight: bold;
+                font-size: 32rpx;
+                color: #000000;
+            }
+            .ste-function-list-subhead {
+                font-size: 24rpx;
+                color: #ac7900;
+                line-height: 44rpx;
+                height: 44rpx;
+                background: rgba(252, 222, 151, 0.23);
+                border-radius: 22rpx;
+                padding: 0 12rpx;
+                margin-left: 18rpx;
+            }
+        }
+        .ste-function-list-header-r {
+            font-size: 24rpx;
+            color: #353535;
+        }
+    }
+    .ste-function-list-content {
+        background-color: var(--ste-function-list-content-bg);
+        padding: 24rpx 20rpx 20rpx 20rpx;
+        margin-top: 24rpx;
+        border-radius: 12rpx;
+    }
+}
+</style>

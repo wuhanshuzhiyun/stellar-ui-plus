443b47d1ffddd8dc2fec5551ea67055bce337025
徐亚军 <850674935@qq.com>
2025-06-17 09:53:01 +0800

    feat(save): save

--- src/uni_modules/stellar-ui-plus/components/ste-app-update/props.ts
+++ src/uni_modules/stellar-ui-plus/components/ste-app-update/props.ts
@@ -12,6 +12,6 @@ export default {
     /** API地址 */
     apiUrl: {
         type: String,
-        default: () => 'https://zboa.whzb.com/inte-cloud-prod/blade-system/api/inte/client/ver/currentDetail',
+        default: () => 'https://zboa.whzb.com/inte-cloud-dev/blade-system/api/inte/client/ver/currentDetail',
     },
 };

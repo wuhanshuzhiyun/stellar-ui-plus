ba8e5725b1c8dd7989bc174408c2ae1ca90fcc63
付宇威 <f6512098@163.com>
2025-11-17 14:08:43 +0800

    修复表格动态展示列后显示报错

--- src/uni_modules/stellar-ui-plus/components/ste-table-column/props.ts
+++ src/uni_modules/stellar-ui-plus/components/ste-table-column/props.ts
@@ -1,18 +1,18 @@
-import type { ExtractPropTypes, PropType } from 'vue'
-import type { AlignType } from '../../types'
-import type { SelectColType } from '../ste-table/types'
+import type { ExtractPropTypes, PropType } from 'vue';
+import type { AlignType } from '../../types';
+import type { SelectColType } from '../ste-table/types';
 
 const tableColumnProps = {
-  type: { type: String as PropType<SelectColType>, default: '' },
-  customKey: { type: String, default: '' },
-  label: { type: String, default: '' },
-  prop: { type: String, default: '' },
-  width: { type: [String, Number], default: '' },
-  minWidth: { type: String, default: '' },
-  align: { type: String as PropType<AlignType>, default: 'left' },
-  headerAlign: { type: String as PropType<AlignType>, default: 'left' },
-}
+    type: { type: String as PropType<SelectColType>, default: '' },
+    customKey: { type: String, default: '' },
+    label: { type: String, default: '' },
+    prop: { type: String, default: '' },
+    width: { type: [String, Number], default: '' },
+    minWidth: { type: [String, Number], default: '' },
+    align: { type: String as PropType<AlignType>, default: 'left' },
+    headerAlign: { type: String as PropType<AlignType>, default: 'left' },
+};
 
-export type TableColumnProps = ExtractPropTypes<typeof tableColumnProps>
+export type TableColumnProps = ExtractPropTypes<typeof tableColumnProps>;
 
-export default tableColumnProps
+export default tableColumnProps;

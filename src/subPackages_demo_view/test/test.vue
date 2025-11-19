64f0ebcb36b4fa834736cf61b84f6211fffa8bf9
付宇威 <f6512098@163.com>
2025-11-19 11:38:02 +0800

    update

--- src/subPackages_demo_view/test/test.vue
+++ src/subPackages_demo_view/test/test.vue
@@ -1,11 +1,23 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 const rows = ref([
-    { name: '张三', birth: '2023.12.31', sex: '男', state: 1 },
-    { name: '李四', birth: '2024.01.01', sex: '女', state: 2 },
-    { name: '王五', birth: '2024.11.01', sex: '女', state: 1 },
-    { name: '赵六', birth: '2024.11.01', sex: '女', state: 2 },
-    { name: '王七', birth: '2024.01.01', sex: '男', state: 1 },
+    { name: '张三', birth: '2023.12.31', sex: '男', state: 1, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
+    { name: '[测试复杂题型]测试时间区间', birth: '2024.01.01', sex: '女', state: 2, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
+    { name: '王五', birth: '2024.11.01', sex: '女', state: 1, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
+    { name: '赵六', birth: '2024.11.01', sex: '女', state: 2, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
+    { name: '王七', birth: '2024.01.01', sex: '男', state: 1, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
+
+    { name: '王五', birth: '2024.11.01', sex: '女', state: 1, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
+    { name: '赵六', birth: '2024.11.01', sex: '女', state: 2, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
+    { name: '王七', birth: '2024.01.01', sex: '男', state: 1, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
+    { name: '[测试复杂题型]测试时间区间', birth: '2024.01.01', sex: '女', state: 2, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
+    { name: '[测试复杂题型]测试时间区间', birth: '2024.01.01', sex: '女', state: 2, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
+    { name: '[测试复杂题型]测试时间区间', birth: '2024.01.01', sex: '女', state: 2, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
+    { name: '[测试复杂题型]测试时间区间', birth: '2024.01.01', sex: '女', state: 2, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
+    { name: '[测试复杂题型]测试时间区间', birth: '2024.01.01', sex: '女', state: 2, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
+    { name: '王七', birth: '2024.01.01', sex: '男', state: 1, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
+    { name: '王七', birth: '2024.01.01', sex: '男', state: 1, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
+    { name: '王七', birth: '2024.01.01', sex: '男', state: 1, remark: '1243123124sdfsdfadsg地方阿萨德发谁说的嘎达松冈祯丞V型出v' },
 ]);
 
 const colorConfig = {
@@ -27,22 +39,25 @@ const colorConfig = {
                         <ste-icon code="&#xe689;" :size="60" :color="'#e1e'"></ste-icon>
                     </view>
                     <view style="width: 100%">
-                        <ste-table :data="rows" :selectionIconColor="colorConfig">
-                            <template v-slot="{ row }">
-                                <ste-table-column label="选择" type="checkbox" align="center" customKey="checkbox"></ste-table-column>
-                                <ste-table-column label="姓名" prop="name"></ste-table-column>
-                                <ste-table-column label="生日" prop="birth"></ste-table-column>
-                                <ste-table-column label="性别" prop="sex"></ste-table-column>
-                            </template>
-                        </ste-table>
-                        <ste-table :data="rows" :selectionIconColor="colorConfig">
+                        <scroll-view scroll-y height="500" class="scroll-view-box">
+                            <ste-table :data="rows" :selectionIconColor="colorConfig" isPopover>
+                                <template v-slot="{ row }">
+                                    <ste-table-column label="选择" type="checkbox" align="center" customKey="checkbox"></ste-table-column>
+                                    <ste-table-column label="姓名" prop="name" :width="100"></ste-table-column>
+                                    <ste-table-column label="生日" prop="birth"></ste-table-column>
+                                    <ste-table-column label="性别" prop="sex"></ste-table-column>
+                                    <ste-table-column label="备注" prop="remark"></ste-table-column>
+                                </template>
+                            </ste-table>
+                        </scroll-view>
+                        <!-- <ste-table :data="rows" :selectionIconColor="colorConfig">
                             <template v-slot="{ row }">
                                 <ste-table-column label="选择" type="radio" align="center" customKey="checkbox"></ste-table-column>
                                 <ste-table-column label="姓名" prop="name"></ste-table-column>
                                 <ste-table-column label="生日" prop="birth"></ste-table-column>
                                 <ste-table-column label="性别" prop="sex"></ste-table-column>
                             </template>
-                        </ste-table>
+                        </ste-table> -->
                     </view>
 
                     <view>
@@ -61,6 +76,10 @@ const colorConfig = {
             .item-block {
                 > view {
                     margin: 0 8px 8px 0;
+                }
+
+                .scroll-view-box {
+                    height: 500rpx;
                 }
             }
         }
